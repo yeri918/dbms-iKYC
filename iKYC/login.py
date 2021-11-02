0ceaddcf65caf0eab16191cc8324696647ed3aac
yeri918 <parkyeri918@gmail.com>
2021-11-02 23:56:37 +0800

    deleted photos

--- iKYC/login.py
+++ iKYC/login.py
@@ -11,11 +11,11 @@ Right_Column = [[sg.Text(' '*10), sg.Button('Forgot email')],
 layout = [[sg.Text('Email : '), sg.InputText(key='in1', do_not_clear=False)],
           [sg.Text('Password : '), sg.InputText(
               key='in2', do_not_clear=False, password_char='*')],
-          [sg.Button(' Log In ')],
+          [sg.Button('Log In')],
           [sg.Text('_' * 60)],
           [sg.Column(Left_Column), sg.VSeperator(), sg.Column(Right_Column)]]
 
-window = sg.Window('Login', layout, margins=(20, 40))
+window = sg.Window('Log In', layout, margins=(20, 40))
 
 while True:
     event, values = window.read()
