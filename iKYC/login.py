5763ff75586905a26079283dc6cef244b2d67f8c
yeri918 <parkyeri918@gmail.com>
2021-11-02 21:01:19 +0800

    login, signup

---
+++ iKYC/login.py
@@ -0,0 +1,26 @@
+import PySimpleGUI as sg
+import signup
+
+
+Left_Column = [[sg.Text('No account yet?'), sg.Text(' '*10)],
+               [sg.Button('Sign Up')]]
+
+Right_Column = [[sg.Text(' '*10), sg.Button('Forgot email')],
+                [sg.Text(' '*10), sg.Button('Forgot password')]]
+
+layout = [[sg.Text('Email : '), sg.InputText(key='in1', do_not_clear=False)],
+          [sg.Text('Password : '), sg.InputText(
+              key='in2', do_not_clear=False, password_char='*')],
+          [sg.Button(' Log In ')],
+          [sg.Text('_' * 60)],
+          [sg.Column(Left_Column), sg.VSeperator(), sg.Column(Right_Column)]]
+
+window = sg.Window('Login', layout, margins=(20, 40))
+
+while True:
+    event, values = window.read()
+    if(event == "Sign Up"):
+        signup.signup()
+
+    if event == "EXIT" or event == sg.WIN_CLOSED:
+        break
